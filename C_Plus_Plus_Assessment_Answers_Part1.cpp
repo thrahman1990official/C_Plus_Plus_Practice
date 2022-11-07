e2ec7bd0c611684e49aa08d5c55ead62a68228a5
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-11-07 13:55:47 -0800

    Create C_Plus_Plus_Assessment_Answers_Part1.cpp

---
+++ C_Plus_Plus_Assessment_Answers_Part1.cpp
@@ -0,0 +1,8 @@
+# https://technorj.com/linkedin-skill-assessment-c-skill-assessment-answers/
+# What is printed from this code?
+vector<int> v(22);
+bool b = (v[6]);
+printf(“%d”, !b);
+# Answer: 1
+
+# 
